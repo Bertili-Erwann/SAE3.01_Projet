7f75478c784b8a8a4a71b1841cdfeeecc4c886e0
Arsamerzoev Magomed <o22401211@or-iut-i007-f09.iut45.univ-orleans.fr>
2025-10-22 14:30:46 +0200

    Finission de la base de donnée avec les triggers, petit jeu de donnée dans data/data.yml, il reste les tests unitaire et la documentation à faire

--- escrimeBlois/commands.py
+++ escrimeBlois/commands.py
@@ -4,94 +4,100 @@ from hashlib import sha256
 from .models import Personne
 from .app import app, db
 from sqlalchemy import func
+import datetime
+
 lg = logging.getLogger(__name__)
 
 
-# @app.cli.command()
-# @click.argument('filename')
-# def loaddb(filename):
-#     """Creates the tables and populates them with data."""
-#     # recréer la base
-#     db.drop_all()
-#     db.create_all()
+@app.cli.command()
+@click.argument('filename')
+def loaddb(filename):
+    """Creates the tables and populates them with data."""
+    # recréer la base
+    db.drop_all()
+    db.create_all()
 
-#     import yaml
-#     from .models import Auteur, Livre
+    import yaml
+    from .models import Personne, Evenement, Classer, Formulaire, Repondre, Article, Inscription
 
-#     with open(filename, 'r', encoding='utf-8') as file:
-#         lesLivres = yaml.safe_load(file) or []
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = yaml.safe_load(file) or []
 
-#     # helper to get a sensible PK attribute name from an author object
-#     def _get_id(obj):
-#         for attr in ('id', 'idA', 'id_auteur', 'idAuteur', 'auteur_id'):
-#             if hasattr(obj, attr):
-#                 return getattr(obj, attr)
-#         return None
+    for pers in data["personnes"]:
+        date_naissance = None
+        if pers.get('date_naissance'):
+            date_naissance = datetime.date.fromisoformat(
+                pers['date_naissance'])
+        personne = Personne(id_personne=pers['id_personne'],
+                            mdp=pers['mdp'],
+                            nom_personne=pers['nom_personne'],
+                            prenom_personne=pers['prenom_personne'],
+                            email_personne=pers['email_personne'],
+                            sexe=pers.get('sexe'),
+                            adresse=pers.get('adresse'),
+                            date_naissance=date_naissance,
+                            etudiant=pers.get('etudiant'),
+                            arme_principale=pers.get('arme_principale'),
+                            niveau=pers.get('niveau'),
+                            role=pers['role'])
+        db.session.add(personne)
+        db.session.commit()
 
-#     # première passe : création de tous les auteurs (évite doublons)
-#     lesAuteurs = {}
-#     for livre in lesLivres:
-#         auteur_name = livre.get('author') or livre.get('auteur')
-#         if not auteur_name:
-#             continue
-#         if auteur_name in lesAuteurs:
-#             continue
-#         auteur_obj = Auteur(nom=auteur_name) if hasattr(Auteur, 'nom') else Auteur(name=auteur_name)
-#         db.session.add(auteur_obj)
-#         # flush pour obtenir un id si nécessaire sans committer encore
-#         db.session.flush()
-#         lesAuteurs[auteur_name] = auteur_obj
-#     db.session.commit()
+    for ev in data["evenements"]:
+        date_evenement = datetime.date.fromisoformat(ev['date'])
+        evenement = Evenement(id_evenement=ev['id_evenement'],
+                              date=date_evenement,
+                              heure=ev['heure'],
+                              categorie=ev.get('categorie'),
+                              lieu=ev['lieu'],
+                              description=ev['description'],
+                              niveau=ev.get('niveau'),
+                              discipline=ev.get('discipline'),
+                              cooperative=ev.get('cooperative'),
+                              type_evenement=ev['type_evenement'])
+        db.session.add(evenement)
+        db.session.commit()
 
-#     # deuxième passe : création de tous les livres
-#     for livre in lesLivres:
-#         auteur_name = livre.get('author') or livre.get('auteur')
-#         auteur_obj = lesAuteurs.get(auteur_name)
-#         if not auteur_obj:
-#             continue
+    for insc in data.get("inscriptions", []):
+        inscription = Inscription(id_inscription=insc['id_inscription'],
+                                  id_evenement=insc['id_evenement'])
+        db.session.add(inscription)
+        db.session.commit()
 
-#         # construire kwargs en essayant plusieurs noms de champs courants
-#         kwargs = {}
-#         if 'price' in livre:
-#             if hasattr(Livre, 'prix'):
-#                 kwargs['prix'] = livre.get('price')
-#             elif hasattr(Livre, 'price'):
-#                 kwargs['price'] = livre.get('price')
-#         if 'title' in livre:
-#             if hasattr(Livre, 'titre'):
-#                 kwargs['titre'] = livre.get('title')
-#             elif hasattr(Livre, 'title'):
-#                 kwargs['title'] = livre.get('title')
-#         if 'url' in livre:
-#             if hasattr(Livre, 'url'):
-#                 kwargs['url'] = livre.get('url')
-#         if 'img' in livre:
-#             if hasattr(Livre, 'img'):
-#                 kwargs['img'] = livre.get('img')
+    for clas in data["classers"]:
+        classer = Classer(id_competition=clas['id_competition'],
+                          id_inscription=clas['id_inscription'],
+                          point=clas.get('point'))
+        db.session.add(classer)
+        db.session.commit()
 
-#         # essayer d'attacher l'auteur soit par clé étrangère soit par relation
-#         auteur_pk = _get_id(auteur_obj)
-#         if auteur_pk is not None:
-#             # trouver le nom probable du champ FK sur Livre
-#             if hasattr(Livre, 'auteur_id'):
-#                 kwargs['auteur_id'] = auteur_pk
-#             elif hasattr(Livre, 'id_auteur'):
-#                 kwargs['id_auteur'] = auteur_pk
-#             elif hasattr(Livre, 'auteurId'):
-#                 kwargs['auteurId'] = auteur_pk
-#             else:
-#                 # dernier recours : passer l'objet relationnel si le modèle l'accepte
-#                 kwargs['auteur'] = auteur_obj
-#         else:
-#             kwargs['auteur'] = auteur_obj
+    for form in data["formulaires"]:
+        formul = Formulaire(id_formulaire=form['id_formulaire'],
+                            nom_auteur=form['nom_auteur'],
+                            prenom_auteur=form['prenom_auteur'],
+                            email_auteur=form['email_auteur'],
+                            objet=form['objet'],
+                            message=form['message'])
+        db.session.add(formul)
+        db.session.commit()
 
-#         livre_obj = Livre(**kwargs)
-#         db.session.add(livre_obj)
+    for rep in data['repondre']:
+        reponse = Repondre(id_responsable=rep['id_responsable'],
+                           id_formulaire=rep['id_formulaire'])
+        db.session.add(reponse)
+        db.session.commit()
 
-#     db.session.commit()
-#     lg.warning('Database initialized!')
-
-
+    for art in data['articles']:
+        date_publication = datetime.date.fromisoformat(art['date_publication'])
+        article = Article(id_article=art['id_article'],
+                          titre=art['titre'],
+                          date_publication=date_publication,
+                          description=art['description'],
+                          categorie=art['categorie'],
+                          commentable=art['commentable'],
+                          responsable_id=art['responsable_id'])
+        db.session.add(article)
+        db.session.commit()
 
 
 @app.cli.command()
@@ -105,7 +111,7 @@ def maxutilisateur() -> int:
     """donne l'id de l'utilisateur le plus grand de la BD, 0 si il n'y en à pas
     """
     max_id = db.session.query(func.max(Personne.id_personne)).scalar()
-    return (int(max_id)+1) if max_id is not None else 1
+    return (int(max_id) + 1) if max_id is not None else 1
 
 
 @app.cli.command()
@@ -117,23 +123,21 @@ def maxutilisateur() -> int:
 def nouvpers(nom, prenom, role_user, pwd, mail):
     """Ajoute un nouveau membre dans la base de donnée 
     """
-    
+
     if Personne.query.filter_by(email_personne=mail).first():
         lg.warning('User %s existe déjà', mail)
         return
     m = sha256()
     m.update(pwd.encode('utf-8'))
-    
+
     pers = Personne(id_personne=maxutilisateur(),
-                      mdp=m.hexdigest(),
-                      role=role_user,
-                      nom_personne=nom,
-                      prenom_personne=prenom,
-                      email_personne=mail)
+                    mdp=m.hexdigest(),
+                    role=role_user,
+                    nom_personne=nom,
+                    prenom_personne=prenom,
+                    email_personne=mail)
 
     db.session.add(pers)
     db.session.commit()
 
-    lg.info('Utilisateur %s crée',prenom)
-
-
+    lg.info('Utilisateur %s crée', prenom)
--- escrimeBlois/models.py
+++ escrimeBlois/models.py
@@ -12,39 +12,84 @@ def load_user(id):
 class Personne(UserMixin, db.Model):
     id_personne = db.Column(db.Integer, primary_key=True)
     mdp = db.Column(db.String(64))
-    role = db.Column(db.String(6))
     nom_personne = db.Column(db.String(64))
     prenom_personne = db.Column(db.String(64))
-    email_personne = db.Column(db.String(64), primary_key=True)
+    email_personne = db.Column(db.String(64))
+    sexe = db.Column(db.String(1))
+    adresse = db.Column(db.String(64))
+    date_naissance = db.Column(db.Date)
+    etudiant = db.Column(db.Boolean)
+    arme_principale = db.Column(db.String(30))
+    niveau = db.Column(db.String(20))
+    role = db.Column(db.String(10))
 
+    @validates('role')
+    def validate_role(self, key, value):
+        match value:
+            case "membre" | "responsable":
+                if self.etudiant is None or self.arme_principale is None or self.niveau is None:
+                    raise ValueError(
+                        f"Le  '{value}' n'a pas rempli un des champs requis")
+            case "personne" | "admin":
+                if self.etudiant is not None or self.arme_principale is not None or self.niveau is not None:
+                    raise ValueError(f"'{value}' a des informations en trop")
+        return value 
 
 class Evenement(db.Model):
     id_evenement = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
     heure = db.Column(db.Integer)
-    type_evenement = db.Column(db.String(64))
+    categorie = db.Column(db.String(30))
     lieu = db.Column(db.String(64))
     description = db.Column(db.String(255))
 
+    niveau = db.Column(db.String(10))
+    discipline = db.Column(db.String(60))
+    cooperative = db.Column(db.String(60))
+
+    type_evenement = db.Column(db.String(64))
+
+    @validates('type_evenement')
+    def validate_attributs_evenement(self, key, value):
+        match value:
+            case "competition":
+                if self.niveau is None or self.discipline is None or self.cooperative is None:
+                    raise ValueError(
+                        f"'{value}' n'a pas rempli un des champs requis")
+            case _:
+                if self.niveau is not None or self.discipline is not None or self.cooperative is not None:
+                    raise ValueError(f"'{value}' a des informations en trop")
+        return value
     def __repr__(self):
-        return "<Evenement (%d) %s>" % (self.id_evenement, self.type_evenement)
+        return f"<Evenement id={self.id_evenement!r} type={self.type_evenement!r} date={self.date!r} heure={self.heure!r} lieu={self.lieu!r}>"
+
+    def __str__(self):
+        return f"{self.type_evenement}"
+
+
+class Inscription(db.Model):
+    id_inscription = db.Column(db.Integer, primary_key=True)
+    id_evenement = db.Column(db.Integer,
+                             db.ForeignKey("evenement.id_evenement"))
 
 
 class Classer(db.Model):
     id_competition = db.Column(db.Integer,
-                               db.ForeignKey("evenement.id_evenement"),primary_key=True)
+                               db.ForeignKey("inscription.id_inscription"),
+                               primary_key=True)
     id_inscription = db.Column(db.Integer,
-                               db.ForeignKey("evenement.id_evenement"),primary_key=True)
+                               db.ForeignKey("evenement.id_evenement"),
+                               primary_key=True)
+    point = db.Column(db.Integer)
 
-    @validates('id_competition', 'id_inscription')
+    @validates('id_competition')
     def validate_evenement_type(self, key, value):
         ev = Evenement.query.get(value)
         if ev is None:
             raise ValueError(f"Événement {value} introuvable")
-        val_attendu = 'competition' if key == 'id_competition' else 'inscription'
-        if ev.type_evenement != val_attendu:
+        if ev.type_evenement != 'competition':
             raise ValueError(
-                f"L'événement {value} doit être de type '{val_attendu}'")
+                f"L'événement {value} doit être de type competition ")
         return value
 
 
@@ -58,18 +103,21 @@ class Formulaire(db.Model):
 
 
 class Repondre(db.Model):
-    id_responsable = db.Column(db.Integer,db.ForeignKey("personne.id_personne"),primary_key=True)
-    id_formulaire = db.Column(db.Integer,db.ForeignKey("formulaire.id_formulaire"),primary_key=True)
+    id_responsable = db.Column(db.Integer,
+                               db.ForeignKey("personne.id_personne"),
+                               primary_key=True)
+    id_formulaire = db.Column(db.Integer,
+                              db.ForeignKey("formulaire.id_formulaire"),
+                              primary_key=True)
 
     @validates('id_responsable')
     def validate_responsable_type(self, key, value):
         pers = Personne.query.get(value)
         if pers is None:
             raise ValueError(f"La personne {value} est introuvable")
-        role_attendu = "resp"
-        if pers.role != role_attendu:
+        if pers.role != 'responsable':
             raise ValueError(
-                f"La personne {value} doit être de type '{role_attendu}' vous avez le type {pers.role}"
+                f"La personne {value} doit être de type 'responsable' vous avez le type {pers.role}"
             )
         return value
 
@@ -78,11 +126,12 @@ class Article(db.Model):
     id_article = db.Column(db.Integer, primary_key=True)
     titre = db.Column(db.String(64))
     date_publication = db.Column(db.Date)
-    contenu = db.Column(db.String(1000))
+    description = db.Column(db.String(1000))
     categorie = db.Column(db.String(20))
     commentable = db.Column(db.Boolean)
     responsable_id = db.ForeignKey("personne.id_personne")
-    responsable = None 
+    responsable = None
+
     @validates('id_responsable')
     def validate_responsable_type(self, key, value):
         pers = Personne.query.get(value)
@@ -94,6 +143,6 @@ class Article(db.Model):
                 f"La personne {value} doit être de type '{role_attendu}' vous avez le type {pers.role}"
             )
         self.responsable = db.relationship("Personne",
-                                      backref=db.backref("articles",
-                                                         lazy="dynamic"))
+                                           backref=db.backref("articles",
+                                                              lazy="dynamic"))
         return value
