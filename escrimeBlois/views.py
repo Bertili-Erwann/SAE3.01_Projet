9a3849240516436249ef8278d0bb3bf4f9424d84
Arsamerzoev Magomed <o22401211@or-iut-i007-f09.iut45.univ-orleans.fr>
2025-11-03 12:52:41 +0100

    finission du front de la page de l'historique

--- escrimeBlois/views.py
+++ escrimeBlois/views.py
@@ -5,3 +5,6 @@ from .app import app
 @app.route('/index/')
 def index():
         return render_template("index.html")
+@app.route('/historique/')
+def historique():
+        return render_template('historique.html')
