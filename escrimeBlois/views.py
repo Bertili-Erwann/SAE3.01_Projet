2f4f2c1640c572b70f5666fe4ab3d1769fbdea55
tdanzin <titouan.danzin@etu.univ-orleans.fr>
2025-11-05 08:41:06 +0100

    Conception de la page de gestion des formulaires pour le Responsable.

--- escrimeBlois/views.py
+++ escrimeBlois/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from .app import app
+from escrimeBlois.models import Formulaire
 
 @app.route('/')
 @app.route('/index/')
@@ -12,3 +13,12 @@ def login():
 @app.route('/historique/')
 def historique():
         return render_template('historique.html')
+
+@app.route('/nav_responsable/')
+def nav_resp():
+        return render_template('nav_responsable.html')
+
+@app.route('/gestion_formulaire/')
+def gest_form():
+        lesFormulaires = Formulaire.query.all()
+        return render_template('gestion_formulaire.html', formulaires = lesFormulaires)
